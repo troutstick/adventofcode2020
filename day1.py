f89e4d7d7b4c298d51e81b6f97cebf28f05ed302
Justin Thein <jthein@berkeley.edu>
2020-12-01 00:16:21 -0800

    finish day 1

--- day1.py
+++ day1.py
@@ -1,7 +1,22 @@
 with open("inputs/day1.txt", "r") as file:
     L = [int(line) for line in file]
+    S = set(L)
     length = len(L)
+    for n in S:
+        if (2020 - n) in S:
+            print(f"The answer is {n}*{2020 - n} = {n*(2020 - n)}")
+            break
+
+    done = False
     for i in range(length):
         for j in range(i+1,length):
-            if L[i] + L[j] == 2020:
-                print(f"The answer is {L[i]}*{L[j]} = {L[i]*L[j]}")
+            pair_val = L[i] + L[j]
+            if (2020 - pair_val) in S:
+                print(f"The three numbers are {L[i]}, {L[j]}, {2020 - pair_val}, the product of which is {L[i]*L[j]*(2020 - pair_val)}")
+                done = True
+                break
+        if done:
+            break
+
+
+    
