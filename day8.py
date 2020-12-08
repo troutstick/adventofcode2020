f966064d1af45a062635dcd2c8edfbab51cfb89f
Justin Thein <jthein@berkeley.edu>
2020-12-07 22:43:53 -0800

    restructure prog

--- day8.py
+++ day8.py
@@ -1,50 +1,45 @@
-pointer = 0
-accumulator = 0
-
-def nop(num):
-    global pointer
-    pointer += 1
-
-def acc(num):
-    global pointer
-    global accumulator
-    pointer += 1
-    accumulator += num
-
-def jmp(num):
-    global pointer
-    pointer += num
-
-func_map = {
-    'jmp' : jmp,
-    'nop' : nop,
-    'acc' : acc
-}
-
 def execute(instructions):
-    global pointer
-    global accumulator
-    inf_loop = True
+    
+    def nop(num):
+        nonlocal pointer
+        pointer += 1
+
+    def acc(num):
+        nonlocal pointer
+        nonlocal accumulator
+        pointer += 1
+        accumulator += num
+
+    def jmp(num):
+        nonlocal pointer
+        pointer += num
+
+    decode_op = {
+        'jmp' : jmp,
+        'nop' : nop,
+        'acc' : acc
+    }
+
+    pointer = 0
+    accumulator = 0
+
     visited = set()
     while pointer not in visited:
         visited.add(pointer)
-        operator, arg = instructions[pointer]
-        func_map[operator](int(arg))
+        op, arg = instructions[pointer]
+        decode_op[op](int(arg))
         if pointer == len(instructions):
-            inf_loop = False
-            break
-    rv = accumulator
-    pointer = 0
-    accumulator = 0
-    return rv, inf_loop
+            return accumulator, False
+    return accumulator, True
 
-def fix_program(program, orig, new):
-    for num in filter(lambda i: program[i][0] == orig, range(len(program))):
+def fix_program(program, old_op, new_op):
+    for num in filter(lambda i: program[i][0] == old_op, range(len(program))):
         new_program = [list(i) for i in program]
-        new_program[num][0] = new
-        acc_val, inf_loop = execute(new_program)
+        new_program[num][0] = new_op
+        exit_val, inf_loop = execute(new_program)
         if not inf_loop:
-            print(f"The fixed program exited with value {acc_val}")
+            print(f"The fixed program exited with value {exit_val}")
+            break
 
 with open('inputs/day8.txt','r') as file:
     program = [line.split() for line in file]
