e4a7fb8c27b6462245bdd68a68e79623651f3670
Justin Thein <jthein@berkeley.edu>
2020-12-10 23:38:09 -0800

    day 11 cleanup

--- day11.py
+++ day11.py
@@ -1,25 +1,19 @@
-def count_adjacent(state, row, col):
-    count = 0
-    for i in range(max(row-1, 0), min(row+2, len(state))):
-        for j in range(max(col-1, 0), min(col+2, len(state[0]))):
-            if not (i == row and j == col):
-                if state[i][j] == '#':
-                    count += 1
-    return count
+with open('inputs/day11.txt', 'r') as file:
+    state = [[char for char in line.strip()] for line in file]
 
-def count_far(state, row, col):
-    def in_bounds(r,c):
-        return r >= 0 and c >= 0 and r < len(state) and c < len(state[0])
+num_rows = len(state)
+num_cols = len(state[0])
 
-    increments = [-1,0,1]
+def count(state, row, col, count_far):
+    in_bounds = lambda r,c: r >= 0 and c >= 0 and r < num_rows and c < num_cols
     count = 0
-    for i in increments:
-        for j in increments:
+    for i in range(-1,2):
+        for j in range(-1,2):
             if i or j:
-                x = row + i
+                x = row + i # so it doesn't count self
                 y = col + j
                 while in_bounds(x,y):
-                    if state[x][y] == '.':
+                    if count_far and state[x][y] == '.':
                         x += i
                         y += j
                     elif state[x][y] == '#':
@@ -29,23 +23,22 @@ def count_far(state, row, col):
                         break
     return count
 
-def game(state, count_func, threshold):
+def game(state, count_far, threshold):
     while True:
         next_state = [list(s) for s in state]
-        for i in range(len(state)):
-            for j in range(len(state[0])):
-                if state[i][j] == '#' and count_func(state, i, j) >= threshold:
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if state[i][j] == '#' and count(state, i, j, count_far) >= threshold:
                         next_state[i][j] = 'L'
-                elif state[i][j] == 'L' and count_func(state, i, j) == 0:
+                elif state[i][j] == 'L' and count(state, i, j, count_far) == 0:
                         next_state[i][j] = '#'
 
-        if set(map(tuple, state)) == set(map(tuple, next_state)): # break if stable
-            break
+        if set(map(tuple, state)) == set(map(tuple, next_state)):
+            break # break if stable
         state = next_state
 
-    return sum([1 for sublist in state for i in sublist if i == '#']) # count num occupied
-
-with open('inputs/day11.txt', 'r') as file:
-    state = [[char for char in line.strip()] for line in file]
-    print(f"The answer to part 1 is {game(state, count_adjacent, 4)}")
-    print(f"The answer to part 2 is {game(state, count_far, 5)}")
+    # count num occupied
+    return len([i for sublist in state for i in sublist if i == '#']) 
+    
+print(f"The answer to part 1 is {game(state, False, 4)}")
+print(f"The answer to part 2 is {game(state, True, 5)}")
