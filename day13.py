b6d13ddc9550cb26c9b6ae190b1c94b122a805ba
Justin Thein <jthein@berkeley.edu>
2020-12-24 15:29:41 -0800

    fin day13

--- day13.py
+++ day13.py
@@ -1,3 +1,14 @@
+from functools import reduce
+import operator as op
+
+def crt(div_to_modulus, M):
+    t = 0
+    for a, m in div_to_modulus:
+        M_div_m = M // m
+        b = M_div_m * pow(M_div_m, -1, m)  # inverse mod
+        t = (t + (a * b)) % M
+    return t
+    
 with open('inputs/day13.txt', 'r') as f:
     depart_time = int(f.__next__())
     buses = [x.strip() for x in f.__next__().split(',')]
@@ -6,5 +17,7 @@ with open('inputs/day13.txt', 'r') as f:
     min_time = min(wait_times)
     print(f"The answer to part 1 is {min_time * wait_times[min_time]}")
 
-    bus_indexes = {int(buses[i]) : i for i in range(len(buses)) if buses[i] != 'x'}
-    print(bus_indexes)
+    div_to_modulus = [(-i, int(buses[i])) for i in range(len(buses)) if buses[i] != 'x']
+    M = reduce(op.mul, buses_in_service)
+
+    print(f"The answer to part 2 is {crt(div_to_modulus, M)}")
