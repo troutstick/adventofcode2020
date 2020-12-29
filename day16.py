3529afaed445e00d6c2f33c2d8cd03fb4a9ab866
Justin Thein <jthein@berkeley.edu>
2020-12-28 16:35:00 -0800

    work on d16p2

--- day16.py
+++ day16.py
@@ -15,14 +15,14 @@ def process_fields(string: str):
             return (x >= a and x <= b) or (x >= c and x <= d)
         
         D[f] = name
-    return D
+    return D, len(fields)
 
 
 with open('inputs/day16.txt') as f:
     ins = f.read().split('\n\n')
     tickets = [[int(i) for i in p.findall(line)] for line in ins[2].splitlines()]
     flat_tickets = [item for sublist in tickets for item in sublist]
-    field_dict = process_fields(ins[0])
+    field_dict, num_fields = process_fields(ins[0])
 
 def in_no_fields(i):
     for f in field_dict:
@@ -30,15 +30,31 @@ def in_no_fields(i):
             return False
     return True
 
-def valid_ticket(t):
+def is_valid_ticket(t):
     for i in t:
         if in_no_fields(i):
             return False
     return True
 
+def single_true(iterable):
+    i = iter(iterable)
+    return any(i) and not any(i)
+
 error_rate = sum(filter(in_no_fields, flat_tickets))
 print(f"The answer to part 1 is {error_rate}")
 
-valid_tickets = [t for t in filter(valid_ticket, tickets)]
-for f, name in field_dict.items():
+field_to_index = {}
+valid_tickets = [t for t in filter(is_valid_ticket, tickets)]
+
+for in_range, field_name in field_dict.items():
+
+    def is_valid_index(i): 
+        return all(map(lambda t: in_range(t[i]), valid_tickets))
     
+    if single_true(map(is_valid_index, range(num_fields))):
+        t = valid_tickets[0]
+        for i in range(num_fields):
+            if in_range(t[i]):
+                field_to_index[field_name] = i
+                break
+        break
