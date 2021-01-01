cdee1813e4763ba1acf80aef97f10f783607cbe2
Justin Thein <jthein@berkeley.edu>
2021-01-01 12:23:47 -0800

    complete day 17

--- day17.py
+++ day17.py
@@ -1,13 +1,9 @@
 from functools import reduce
 from operator import add
 
-with open('inputs/day17.txt', 'r') as f:
-    lines = [line.strip() for line in f]
-
-def neighbor_set(coord):
+def neighbor_set_3d(coord):
     X, Y, Z = coord
-    return set(
-            filter(
+    return set(filter(
                 lambda n: n != coord, 
                 [(x,y,z)
                 for x in range(X-1, X+2) 
@@ -18,8 +14,7 @@ def neighbor_set(coord):
 
 def neighbor_set_4d(coord):
     X, Y, Z, W = coord
-    return set(
-            filter(
+    return set(filter(
                 lambda n: n != coord, 
                 [(x,y,z,w)
                 for x in range(X-1, X+2) 
@@ -29,33 +24,39 @@ def neighbor_set_4d(coord):
             )
         )
 
-active = set()
-for x in range(len(lines)):
-    for y in range(len(lines[0])):
-        if lines[x][y] == '#':
-            active.add((x,y,0))
-
-for _ in range(6):
-    next_active = set()
-
-    all_neighbor_sets = [neighbor_set(pos) for pos in active]
-    all_neighbors = reduce(set.union, all_neighbor_sets)
-
-    for pos, pos_neighbors in zip(active, all_neighbor_sets):
-        num_active_neighbors = reduce(
-            add, 
-            map(lambda p: p in active, pos_neighbors)
+def next_state(next, to_convert, activate_condition, active, neighbors):
+    def num_active_neighbors(pos):
+        return reduce(add, map(lambda p: p in active, neighbors[pos]))
+    
+    return next.union(
+        set(filter(
+                lambda pos: activate_condition(num_active_neighbors(pos)),
+                to_convert
+            )
         )
-        if num_active_neighbors in range(2,4):
-            next_active.add(pos)
+    )
 
-    for pos in all_neighbors:
-        num_active_neighbors = reduce(
-            add,
-            map(lambda p: p in active, neighbor_set(pos))
-        )
-        if num_active_neighbors == 3:
-            next_active.add(pos)
-    active = next_active
+def solve(is_4d, lines):
 
-print(f"The answer to part 1 is {len(active)}")
+    def initial_active(n):
+        return lines[n[0]][n[1]] == '#'
+
+    neighbor_set = neighbor_set_4d if is_4d else neighbor_set_3d
+    starter_coords = [(x,y,0) for x in range(len(lines)) for y in range(len(lines[0]))]
+    if is_4d:
+        starter_coords = [(x,y,z,0) for x,y,z in starter_coords]
+
+    active = set(filter(initial_active, starter_coords))
+    for _ in range(6):
+        next_active = set()
+        neighbors = {pos:neighbor_set(pos) for pos in active}
+        all_neighbors = reduce(set.union, neighbors.values())
+        neighbors.update({pos:neighbor_set(pos) for pos in all_neighbors})
+        next_active = next_state(next_active, active, lambda n: n == 2 or n == 3, active, neighbors)
+        active = next_state(next_active, all_neighbors, lambda n: n == 3, active, neighbors)
+    return len(active)
+
+with open('inputs/day17.txt', 'r') as f:
+    lines = [line.strip() for line in f]
+    print(f"The answer to part 1 is {solve(False, lines)}")
+    print(f"The answer to part 2 is {solve(True, lines)}")
