ecfc520c337277bec6f3d0efdf00360def6425e6
Justin Thein <jthein@berkeley.edu>
2021-01-11 10:28:53 -0800

    fin d19 p1

--- day19.py
+++ day19.py
@@ -1,8 +1,22 @@
+import re
+
 def parse_rule(rule):
     if rule[0] == '"':
         return rule[1] # for matching char
     else:
         return [[int(i) for i in clause.split()] for clause in rule.split('|')]
+
+def re_pattern(num=0):
+    rule = rules[num]
+    if type(rule) is list:
+        s = ''
+        for clause in rule:
+            for i in clause:
+                s += f"({re_pattern(i)})"
+            s += '|'
+        return s
+    else:
+        return rule
 
 with open('inputs/day19.txt', 'r') as f:
     rules, messages = f.read().split('\n\n')
@@ -12,17 +26,5 @@ with open('inputs/day19.txt', 'r') as f:
 # print(rules)
 # print(messages)
 
-
-# print(parse_rule('130 90 | 50 53'))
-
-def match(string, num=0):
-    # consume string
-    rule = rules[num]
-    if type(rule) is list:
-        for clause in rule:
-            str_copy = string[:]
-            for i in clause:
-                str_copy = match(str_copy, i)
-    else:
-        return string[1:] if string[:1] == rule else string
-    pass
+p = re.compile(f"^({re_pattern()})$")
+print(f"The answer to part 1 is {sum(map(lambda s: bool(p.match(s)), messages))}")
