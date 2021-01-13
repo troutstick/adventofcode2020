69f9818c2dbcbb5f92fe8e9f5aff79ee7ce82480
Justin Thein <jthein@berkeley.edu>
2021-01-13 01:05:23 -0800

    work on d19p2

--- day19.py
+++ day19.py
@@ -1,4 +1,4 @@
-import re
+import regex as re
 
 def parse_rule(rule):
     if rule[0] == '"':
@@ -6,25 +6,38 @@ def parse_rule(rule):
     else:
         return [[int(i) for i in clause.split()] for clause in rule.split('|')]
 
-def re_pattern(num=0):
+def re_pattern(num=0, recursive=False):
+    if recursive and num == 0:
+        return f"({re_pattern(42, recursive)})+(?<x>{re_pattern(42, recursive)}(?&x)?{re_pattern(31, recursive)})"
+    elif num == 0:
+        return f"({re_pattern(42)})({re_pattern(42)})({re_pattern(31)})$"
+
     rule = rules[num]
     if type(rule) is list:
         s = ''
         for clause in rule:
             for i in clause:
-                s += f"({re_pattern(i)})"
+                s += f"({re_pattern(i, recursive)})"
             s += '|'
-        return s
+        return s[:-1] # discard final or
     else:
         return rule
 
-with open('inputs/day19.txt', 'r') as f:
+with open('inputs/day19test.txt', 'r') as f:
     rules, messages = f.read().split('\n\n')
     rules = {int(i):parse_rule(rule) 
         for i, rule in [r.split(': ') for r in rules.splitlines()]}
     messages = messages.split()
 # print(rules)
 # print(messages)
+print(re_pattern(0, True))
 
 p = re.compile(f"^({re_pattern()})$")
 print(f"The answer to part 1 is {sum(map(lambda s: bool(p.match(s)), messages))}")
+
+p = re.compile(re_pattern(recursive=True))
+# for m in messages:
+#     if p.match(m):
+#         print(m)
+# above 270
+print(f"The answer to part 2 is {sum(map(lambda s: bool(p.match(s)), messages))}")
