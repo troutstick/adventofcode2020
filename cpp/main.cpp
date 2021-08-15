ec7764b2815a4a5bed8a10b9206ad59e05979f1a
Justin Thein <justin.a.thein@gmail.com>
2021-08-15 00:27:22 -0700

    do day 1 in cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -9,6 +11,35 @@ const string INPUT_PATH = "./inputs/";
 int main() {
     ifstream file;
     file.open(INPUT_PATH + "day1.txt");
-    cout << file.rdbuf();
+    string str;
+    vector<int> nums;
+    unordered_set<int> numset;
+    while (getline(file, str)) {
+        int n = stoi(str);
+        nums.push_back(n);
+    }
+    cout << "the number of nums is " << nums.size() << endl;
+    for (int i = 0; i < nums.size(); i++) {
+        // if set contains key
+        int curr = nums[i];
+        int target = 2020 - curr;
+        if (numset.find(target) != numset.end()) {
+            cout << curr * target << endl;
+            break;
+        } else {
+            numset.insert(curr);
+        }
+    }
+    for (int i = 0; i < nums.size() - 1; i++) {
+        for (int j = i+1; j < nums.size(); j++) {
+            int pair = nums[i] + nums[j];
+            int target = 2020 - pair;
+            if (numset.find(target) != numset.end()) {
+                cout << nums[i] << " " << nums[j] << " " << target << endl;
+                cout << nums[i] * nums[j] * target << endl;
+                return 0;
+            }
+        }
+    }
     return 0;
 }
